b5e2ffb3c10963932a6c4c74b6cc6590d0d9605f
Prokhor <i7poxop@mail.ru>
2019-06-19 10:52:40 +0300

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -1,6 +1,6 @@
 package com.microsoft.demo;
 
-publi class Demo {
+public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
             System.out.println("I am covered");
